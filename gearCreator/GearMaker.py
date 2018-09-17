15a881fec8a3593037bad55c9e1fac71a1b8010e
Yzk-PC\Yzk <bujie02@hotmail.com>
2018-09-18 01:03:40 +0800

    1测试maya model import 路径设置 2.测试string format 语法 3.准备开始进行 GearMaker逻辑编写

---
+++ gearCreator/GearMaker.py
@@ -0,0 +1,8 @@
+import maya.cmds as cmds
+
+##use sys.path.append() to add a path, so that maya could import model
+def makegear(teeth = 5):
+
+    print "Hello Gear!{0}".format(teeth)
+
+print "gearmaker"
