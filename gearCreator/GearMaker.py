84f74a659904faac4697d47b427dadee3b40b036
SofiaRin <bujie02@sina.com>
2018-09-21 00:23:31 +0800

    正在编写changeGear方法，以重新设定齿轮的齿数和长度

--- gearCreator/GearMaker.py
+++ gearCreator/GearMaker.py
@@ -1,8 +1,46 @@
 import maya.cmds as cmds
 
-##use sys.path.append() to add a path, so that maya could import model
-def makegear(teeth = 5):
+##use sys.path.append() to add a path, so that maya could import module
+##import sys
+##sys.path.append it seems need run everyTime before import your module
+def makeGear(teeth = 10, length = 0.5):
+    removeGear()
+    spin = teeth * 2
+    transform, constructor = cmds.polyPipe(subdivisionsAxis=spin, name='Gear')
 
-    print "Hello Gear!{0}".format(teeth)
+    ##calcute sideFace number
+    faceNum = range(spin * 2, spin * 3, 2)
+    #print faceNum
 
-print "gearmaker"
+    ##clean everySelection
+    cmds.select(clear=True)
+    for face in faceNum:
+        print face
+        ##select target faces
+        cmds.select("%s.f[%s]" % (transform, face), add=True) or []
+
+    extrudeAttr = cmds.polyExtrudeFacet(ltz = length)[0]
+    print extrudeAttr
+    print transform, constructor
+    return  transform,constructor,extrudeAttr
+
+def removeGear(name='Gear'):
+    exsitGear = cmds.ls(name)
+    if (exsitGear):
+        cmds.delete(exsitGear)
+        print "delete the former one "
+
+def changeGear(transform,constructor,extrudeAttr,teeth = 5, length = 0.2):
+    spin = teeth*2
+    cmds.polyPipe(constructor, edit = True, subdivisionsAxis=spin)
+    faceNum = range(spin * 2, spin * 3, 2)
+
+    cmds.select(clear=True)
+    for face in faceNum:
+        print face
+        ##select target faces
+        cmds.select("%s.f[%s]" % (transform, face), add=True) or []
+    ##attention this will create another extrude
+    ##we just need to edit the current extrude attr
+    extrudeAttr = cmds.polyExtrudeFacet(ltz = length)[0]
+    return 0
